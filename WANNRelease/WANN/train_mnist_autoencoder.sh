c547767a30de5d43d7a228247bdb1be390958536
Fabio Anderegg <fabio.anderegg@zebbra.ch>
2019-10-25 15:51:01 +0200

    log: append log of autoencoder training

--- WANNRelease/WANN/train_mnist_autoencoder.sh
+++ WANNRelease/WANN/train_mnist_autoencoder.sh
@@ -1,5 +1,5 @@
 today=`date +%Y-%m-%d.%H-%M-%S`
 while :
 do
-	python wann_train.py -p p/mnist_autoencoder.json -o mnist_autoencoder -n 4 | tee log/training_log_autoencoder_$today.txt
+	python wann_train.py -p p/mnist_autoencoder.json -o mnist_autoencoder -n 4 | tee -a log/training_log_autoencoder_$today.txt
 done
