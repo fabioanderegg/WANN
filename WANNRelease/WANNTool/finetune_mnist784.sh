d5b3ec5100c5e47f988f22dc91b43b99187abe33
Fabio Anderegg <fabio.anderegg@zebbra.ch>
2019-10-28 09:31:35 +0100

    finetune models on train dataset

--- WANNRelease/WANNTool/finetune_mnist784.sh
+++ WANNRelease/WANNTool/finetune_mnist784.sh
@@ -1,1 +1,1 @@
-python train.py mnist784test -e 4 -n 8 -t 4 -o cma --sigma_init 0.5
+python train.py mnist784train -e 4 -n 8 -t 4 -o cma --sigma_init 0.5
--- WANNRelease/WANNTool/finetune_mnist_features.sh
+++ WANNRelease/WANNTool/finetune_mnist_features.sh
@@ -1,1 +1,1 @@
-python train.py mnistfeaturestest -e 4 -n 8 -t 4 -o cma --sigma_init 0.5
+python train.py mnistfeaturestrain -e 4 -n 8 -t 4 -o cma --sigma_init 0.5
